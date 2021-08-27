e0fa682ed22dd412fef6c77cb0f9765f7be56809
superduper-cbd <alvinye@gmail.com>
2021-08-26 18:10:39 -0700

    Commited simply-static-1-1630026597

---
+++ wp-content/plugins/zedend-widget/assets/js/custom.js
@@ -0,0 +1,13 @@
+
+  // <!-- Initialize Swiper -->
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
